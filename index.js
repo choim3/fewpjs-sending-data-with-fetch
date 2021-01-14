312e08dd68c0ab01820163e6b1c271f1c0c4c006
choim3 <choim3@spu.edu>
2021-01-14 12:00:32 -0800

    commit tree b0a415328ad2e7893ed66851dbc884b11108d5f5

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+
+fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  });
+
+
+function submitData(name, email) {
+  let formData = {
+    name: name,
+    email: email
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+    })
+    .catch(function(error) {
+      alert("Unauthorized Access");
+      console.log(error.message);
+    });
+}
